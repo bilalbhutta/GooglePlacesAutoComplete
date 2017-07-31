8c3d5e28b3efffeecbe1078972f89147fc06f524
Bilal Bhutta <bilaljaved.bhutta@gmail.com>
2017-07-31 14:51:43 +0800

    Google Places auto complete view controller sample project.

---
+++ GooglePlacesSample/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  GooglePlacesSample
+//
+//  Created by Bilal on 31/7/17.
+//  Copyright © 2017 Bilal. All rights reserved.
+//
+
+import UIKit
+import GooglePlaces
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Present the Autocomplete view controller when the button is pressed.
+    @IBAction func onLaunchClicked(sender: UIButton) {
+        let acController = GMSAutocompleteViewController()
+        acController.delegate = self
+        present(acController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        print("Place address: \(place.formattedAddress)")
+        print("Place attributions: \(place.attributions)")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: \(error)")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // User cancelled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        print("Autocomplete was cancelled.")
+        dismiss(animated: true, completion: nil)
+    }
+}
